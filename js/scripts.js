76da140eb562a344aa13ae61868ccf296f9fd30c
kentonh <kenton@gobananago.com>
2014-09-04 16:21:48 -0500

    fix specs and make quotes objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-
 // Store all quotes in array. Einstein quotes are odd index, Tesla quotes are even index. Nothing in index 0.
-var quotes = ["Want another?","Imagination is more important than knowledge.","I don't care that they stole my idea . . I care that they don't have any of their own","Gravitation is not responsible for people falling in love.","The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane.","I want to know God's thoughts; the rest are details.","The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous centuries of its existence.","The hardest thing in the world to understand is the income tax.","If your hate could be turned into electricity, it would light up the whole world.","Reality is merely an illusion, albeit a very persistent one.","One must be sane to think clearly, but one can think deeply and be quite insane.","The only real valuable thing is intuition.","Of all things, I liked books best.","A person starts to live when he can live outside himself.","I do not think you can name many great inventions that have been made by married men.","God is subtle but he is not malicious.","The present is theirs; the future, for which I really worked, is mine.",]
+var quotes = [
+{ name:"E", text:"Imagination is more important than knowledge."},
+{ name:"T", text:"I don't care that they stole my idea . . I care that they don't have any of their own"},
+{ name:"E", text:"Gravitation is not responsible for people falling in love."},
+{ name:"T", text:"The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane."},
+{ name:"E", text:"I want to know God's thoughts; the rest are details."},
+{ name:"T", text:"The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous centuries of its existence."},
+{ name:"E", text:"The hardest thing in the world to understand is the income tax."},
+{ name:"T", text:"If your hate could be turned into electricity, it would light up the whole world."},
+{ name:"E", text:"Reality is merely an illusion, albeit a very persistent one."},
+{ name:"T", text:"One must be sane to think clearly, but one can think deeply and be quite insane."},
+{ name:"E", text:"The only real valuable thing is intuition."},
+{ name:"T", text:"Of all things, I liked books best."},
+{ name:"E", text:"A person starts to live when he can live outside himself."},
+{ name:"T", text:"I do not think you can name many great inventions that have been made by married men."},
+{ name:"E", text:"God is subtle but he is not malicious."},
+{ name:"T", text:"The present is theirs; the future, for which I really worked, is mine."}];
 
 var randomNumber = Math.floor(Math.random()*(quotes.length - 1)) + 1;
 var quote = quotes[randomNumber];
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,19 @@
 describe('test answers of Einstein', function() {
     it('Should return Einstein Answers of Odd Indexes as correct', function() {
-        einstein(3).should.equal("correct");
+        einstein(3).should.equal('<div class="correct">Correct! <a href="">Want another?</a></div>');
     });
     it('Should return Einstein Answers of Even Indexes as incorrect', function() {
-        einstein(4).should.equal("incorrect");
+        einstein(4).should.equal('<div class="incorrect">Incorrect! <a href="">Want another?</a></div>');
     });
 
 });
 
 describe('test answers of Tesla', function() {
     it('Should return Tesla Answers of Even Indexes as correct', function() {
-        tesla(6).should.equal("correct");
+        tesla(6).should.equal('<div class="correct">Correct! <a href="">Want another?</a></div>');
     });
     it('Should return Tesla Answers of Odd Indexes as incorrect', function() {
-        tesla(5).should.equal("incorrect");
+        tesla(5).should.equal('<div class="incorrect">Incorrect! <a href="">Want another?</a></div>');
     });
 
 });
