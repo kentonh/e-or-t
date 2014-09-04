9e5dc3665471ceb36f51eefc57e9c627677fb4c6
kentonh <kenton@gobananago.com>
2014-09-04 16:41:07 -0500

    reorganize quote objects for better readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 // Store all quotes in array. Einstein quotes are odd index, Tesla quotes are even index. Nothing in index 0.
 var quotes = [
 { name:"E", text:"Imagination is more important than knowledge."},
-{ name:"T", text:"I don't care that they stole my idea . . I care that they don't have any of their own"},
 { name:"E", text:"Gravitation is not responsible for people falling in love."},
-{ name:"T", text:"The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane."},
-{ name:"E", text:"I want to know God's thoughts; the rest are details."},
-{ name:"T", text:"The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous centuries of its existence."},
-{ name:"E", text:"The hardest thing in the world to understand is the income tax."},
-{ name:"T", text:"If your hate could be turned into electricity, it would light up the whole world."},
-{ name:"E", text:"Reality is merely an illusion, albeit a very persistent one."},
-{ name:"T", text:"One must be sane to think clearly, but one can think deeply and be quite insane."},
-{ name:"E", text:"The only real valuable thing is intuition."},
-{ name:"T", text:"Of all things, I liked books best."},
-{ name:"E", text:"A person starts to live when he can live outside himself."},
-{ name:"T", text:"I do not think you can name many great inventions that have been made by married men."},
 { name:"E", text:"God is subtle but he is not malicious."},
-{ name:"T", text:"The present is theirs; the future, for which I really worked, is mine."}];
+{ name:"E", text:"A person starts to live when he can live outside himself."},
+{ name:"E", text:"The only real valuable thing is intuition."},
+{ name:"E", text:"I want to know God's thoughts; the rest are details."},
+{ name:"E", text:"The hardest thing in the world to understand is the income tax."},
+{ name:"E", text:"Reality is merely an illusion, albeit a very persistent one."},
+{ name:"T", text:"If your hate could be turned into electricity, it would light up the whole world."},
+{ name:"T", text:"The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous centuries of its existence."},
+{ name:"T", text:"One must be sane to think clearly, but one can think deeply and be quite insane."},
+{ name:"T", text:"Of all things, I liked books best."},
+{ name:"T", text:"The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane."},
+{ name:"T", text:"I do not think you can name many great inventions that have been made by married men."},
+{ name:"T", text:"The present is theirs; the future, for which I really worked, is mine."},
+{ name:"T", text:"I don't care that they stole my idea . . I care that they don't have any of their own"}];
 
 var randomNumber = Math.floor(Math.random()*(quotes.length - 1)) + 1;
 var quote = quotes[randomNumber];
